4dfec4f737de8bd3b422ce47b9cb6951ff678a37
RedzSoul <lorenzomarinsalda@hotmail.it>
2019-07-07 22:09:39 +0200

    Fixato problema menu iniziale

--- js/funzioni.js
+++ js/funzioni.js
@@ -15,3 +15,23 @@ function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
+
+function funzioneSpoilerButton(pulsante) 
+{
+    if (pulsante.parentNode.parentNode.getElementsByTagName('div')['show'].style.display != '') 
+    { 
+        pulsante.parentNode.parentNode.getElementsByTagName('div')['show'].style.display = '';
+        pulsante.parentNode.parentNode.getElementsByTagName('div')['hide'].style.display = 'none';
+        pulsante.innerText = '';
+        pulsante.value = 'Nascondi'; 
+    }
+    else 
+    { 
+        pulsante.parentNode.parentNode.getElementsByTagName('div')['show'].style.display = 'none';
+        pulsante.parentNode.parentNode.getElementsByTagName('div')['hide'].style.display = '';
+        pulsante.innerText = '';
+        pulsante.value = 'Menu'; 
+    }
+
+}
